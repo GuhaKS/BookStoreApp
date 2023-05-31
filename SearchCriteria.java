b22b20881f8ca49f17e462286f9cbf3348e31aaa
GuhaKS <134485137+GuhaKS@users.noreply.github.com>
2023-05-31 17:11:26 +0530

    Update SearchCriteria.java

--- SearchCriteria.java
+++ SearchCriteria.java
@@ -12,25 +12,32 @@ class SearchCriteria{
     public void setTitle(String title) {
         this.title = title;
     }
+    
     public void setAuthor(String author) {
         this.author = author;
     }
+    
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
+    
     public void setPrice(double minPrice,double maxPrice){
         this.minPrice = minPrice;
         this.maxPrice = maxPrice;
     }
+    
     public List<Book> searchBooksTitle(List<Book> books) {
         return books.stream().filter(book -> book.getTitle().toLowerCase().contains(title.toLowerCase())).collect(Collectors.toList());
     }
+    
     public List<Book> searchBooksByAuthor(List<Book> books) {
         return books.stream().filter(book -> book.getTitle().toLowerCase().contains(author.toLowerCase())).collect(Collectors.toList());
     }
+    
     public List<Book> searchBooksByPublicationYear(List<Book> books) {
         return books.stream().filter(book -> book.getId() == publicationYear).collect(Collectors.toList());
     }
+    
     public List<Book> searchBooksByPriceRange(List<Book> books) {
         return books.stream().filter(book -> book.getPrice()>= minPrice && book.getPrice() <= maxPrice).collect(Collectors.toList());
     }
