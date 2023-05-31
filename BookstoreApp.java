cd9a2ecb737850ae49ec7d3475020810f7edf5ea
GuhaKS <134485137+GuhaKS@users.noreply.github.com>
2023-05-31 17:16:43 +0530

    Update BookstoreApp.java

--- BookstoreApp.java
+++ BookstoreApp.java
@@ -5,7 +5,7 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-class BookstoreApp {
+public class BookstoreApp {
     
     private static Scanner scanner;
     private static BookInventory bookInventory;
